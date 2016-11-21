421e266cdd8856becee0523c229810ed03e31aee
_xiaoxiao7 <_xiaoxiao7@xiaoxiao7deMacBook-Pro.local>
2016-11-21 13:59:24 +0800

    Binary Tree

--- 102_Binary_Tree_Level_Order_Traversal.py
+++ 102_Binary_Tree_Level_Order_Traversal.py
@@ -4,7 +4,6 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
 import Queue
 class Solution(object):
     def levelOrder(self, root):
@@ -12,31 +11,28 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[List[int]]
         """
-        # generate a queue class
         binary_tree = Queue.Queue()
-        # put the value of root into the queue
         binary_tree.put(root)
-        # level counters
         current_level = 1
         next_level = 0
-        #level = 0  
         temp = []
-        output_arrary =[]
+        output = []
         if root == None:
-        	return output_arrary
-        while binary_tree.empty() == false:
-        	node = binary_tree.get()
-        	temp =temp.extend(node.val)
-        	if node.left != None:
-        		binary_tree.put(node.left)
-        		next_level = next_level + 1
-        	if node.right != None:
-        		binary_tree.put(node.right)
-        		next_level = next_level + 1
-        	current_level = current_level - 1
-			if current_level ==0:
-        		current_level = next_level
-        		next_level = 0
-        		output_arrary.append(temp)
-        		temp = []
-        return output_arrary
+            return output
+        while binary_tree.empty() == False:
+            node = binary_tree.get()
+            temp.extend([node.val])
+            if node.left != None:
+                binary_tree.put(node.left)
+                next_level = next_level + 1
+            if node.right != None:
+                binary_tree.put(node.right)
+                next_level = next_level + 1
+                
+            current_level = current_level - 1
+            if current_level == 0:
+                current_level = next_level
+                next_level = 0
+                output.append(temp)
+                temp = []
+        return output
